380eceea104546eac06ddce8c511976aaafbbfe2
Mario Werner <mario.werner@iaik.tugraz.at>
2014-08-06 10:52:39 +0200

    slight refactoring and some comments

--- lib/builddir/utils.rb
+++ lib/builddir/utils.rb
@@ -6,7 +6,7 @@ module Builddir
 	# http://stackoverflow.com/a/5805783
 	#
 	# @return [String] path to the library directory
-	def Builddir.gem_libdir
+	def Builddir.getGemLibdir
 		t = ["#{File.dirname(File.expand_path($0))}/../lib/#{NAME}",
 					"#{Gem.dir}/gems/#{NAME}-#{VERSION}/lib/#{NAME}"]
 		t.each {|i| return i if File.readable?(i) }
